0a690af32058dded082f731413a9345526016818
ronit5036m <ronitshakhari2005@gmail.com>
2026-05-10 20:54:39 +0530

    update:obj name

--- src/stores/useUiStateStore.js
+++ src/stores/useUiStateStore.js
@@ -1,24 +1,3 @@
-// import { create } from "zustand";
-// import { persist } from "zustand/middleware";
-
-// const useUiStateStore = create(
-//   persist(
-//     (set, get) => ({
-//       appName: "Institute-of-knowledge",
-//       isDarkMode: false,
-//       toggleDarkmode: () =>
-//         set((state) => ({
-//           isDarkMode: !state.isDarkMode,
-//         })),
-//     }),
-//     {
-//       name: "theme",
-//     },
-//   ),
-// );
-
-// export default useUiStateStore;
-
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
@@ -30,7 +9,7 @@ const useUiStateStore = create(
       setTheme: (newTheme) => set({ theme: newTheme }),
     }),
     {
-      name: "ui-theme",
+      name: "theme",
     },
   ),
 );
