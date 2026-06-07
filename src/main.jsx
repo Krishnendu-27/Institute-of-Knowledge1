ec522bba50e56b1a16bde2fdec25ca880498aedc
ronit5036m <ronitshakhari2005@gmail.com>
2026-06-07 09:42:01 +0530

    Implement PWA

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,23 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New application updates available! Reload to apply?")) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log(
+      "App resources successfully cached. App ready for offline use.",
+    );
+  },
+});
+
 createRoot(document.getElementById("root")).render(
-  // <StrictMode>
-  <App />,
-  // </StrictMode>,
+  <StrictMode>
+    <App />,
+  </StrictMode>,
 );
