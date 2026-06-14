3fca3e78abd3f240cd324b2751021b320239995e
ronit5036m <ronitshakhari2005@gmail.com>
2026-06-14 10:12:07 +0530

    Update Mobile navbar

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.vda413tibhg"
+    "revision": "0.mb79pp2pf3g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
